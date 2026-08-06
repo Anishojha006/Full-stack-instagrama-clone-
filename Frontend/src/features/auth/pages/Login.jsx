a1a837ce64cfac88b4becb95daf88b57ccbcf6a9
Anishojha006 <anishojha1512@gmail.com>
2026-08-06 18:51:33 +0530

    made login and register page function

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../style/form.scss'
 import { Link } from 'react-router-dom'
+import axios from 'axios';
 const Login = () => {
+   const [username, setusername] = useState("");
+   const [password, setpassword] = useState("")
+
+async function submitHandler(e){
+    e.preventDefault();;
+   await axios.post(" http://localhost:3000/api/auth/login",{
+        username,password
+    },{
+        withCredentials:true
+    }).then((res)=>{
+        console.log(res.data);
+    })
+    setpassword("");
+    setusername("");
+}
+
   return (
     <main>
         <div className="form-container">
             <h1>Login</h1>
-            <form>
-                <input type="text" name='username' placeholder='Enter Username' autoComplete='usename'/>
-                <input type='password' name='password' placeholder='Enter password' autoComplete='current-password'/>
+            <form onSubmit={submitHandler}>
+                <input type="text" name='username' placeholder='Enter Username' autoComplete='usename' value={username} onChange={(e)=>{
+                    setusername(e.target.value);
+                }}/>
+
+                <input type='password' name='password' placeholder='Enter password' autoComplete='current-password' value={password} onChange={(e)=>{
+                    setpassword(e.target.value);
+                }}/>
 
                 <button>Login</button>
             </form>
